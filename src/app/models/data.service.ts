189be75435afe9888d24d5b075ed6ab403d2307d
Moshe Peres <mosheper99@gmail.com>
2019-05-12 14:55:22 -0400

    changing some stuff

--- src/app/models/data.service.ts
+++ src/app/models/data.service.ts
@@ -14,14 +14,14 @@ export class DataService {
 
   // This Gets All the shows from The Api And this function is being called so it fetches data from the api
   getShows(search: string): Observable<Shows[]> {
-    return this.Http.get<Shows[]>( 'http://api.tvmaze.com/search/shows?q=' + search);
+    return this.Http.get<Shows[]>( 'https://api.tvmaze.com/search/shows?q=' + search);
   }
   // this fetches data from the api and returns it to get all the seasons
   getSeasons(showId: string): Observable<Seasons[]> {
-    return this.Http.get<Seasons[]> ('http://api.tvmaze.com/shows/' + showId + '/seasons');
+    return this.Http.get<Seasons[]> ('https://api.tvmaze.com/shows/' + showId + '/seasons');
   }
   // This fetches data from the api and displays all the episodes which it retrieves from the api
   getEpisodes(seasonId: string): Observable<Episodes[]> {
-    return this.Http.get<Episodes[]> ('http://api.tvmaze.com/seasons/' + seasonId + '/episodes');
+    return this.Http.get<Episodes[]> ('https://api.tvmaze.com/seasons/' + seasonId + '/episodes');
   }
 }
